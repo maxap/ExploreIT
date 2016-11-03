301e05db8af1c4acbd5441b1c09491593dda0d46
Lu-Ma-Ni <luca.niese@gmx.de>
2016-11-03 10:10:27 +0100

    AutoCompleteTextView hinzugefügt

--- ProjectSaentis/app/src/main/java/com/saentis/project/projectsntis/SelectionActivity.java
+++ ProjectSaentis/app/src/main/java/com/saentis/project/projectsntis/SelectionActivity.java
@@ -2,12 +2,28 @@ package com.saentis.project.projectsntis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class SelectionActivity extends AppCompatActivity {
+
+    AutoCompleteTextView autoCompleteTextView1, autoCompleteTextView2, autoCompleteTextView3, autoCompleteTextView4;
+    String [] Ingredients;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+        autoCompleteTextView1 = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
+        autoCompleteTextView2 = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView2);
+        autoCompleteTextView3 = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView3);
+        autoCompleteTextView4 = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView4);
+        Ingredients = getResources().getStringArray(R.array.ingredients);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Ingredients);
+        autoCompleteTextView1.setAdapter(adapter);
+        autoCompleteTextView2.setAdapter(adapter);
+        autoCompleteTextView3.setAdapter(adapter);
+        autoCompleteTextView4.setAdapter(adapter);
     }
 }
