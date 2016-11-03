0e0b2ec2b67c897f1b3c1cb0ce6cd45a70dec62a
Max Appel <scheresteinpapier2.0@gmail.com>
2016-11-03 12:47:30 +0100

    Gericht.class updatet

--- ProjectSaentis/app/src/main/java/com/saentis/project/projectsntis/Gericht.java
+++ ProjectSaentis/app/src/main/java/com/saentis/project/projectsntis/Gericht.java
@@ -10,12 +10,8 @@ class Gericht  implements Serializable{
     String[] ingredients;
     String description;
 //test
-    public Gericht(String Gericht, String ingredient1, String ingredient2, String ingredient3, String ingredient4)	{
-        this.Gericht = Gericht;
-        this.ingredients[0] = ingredient1;
-        this.ingredients[1] = ingredient2;
-        this.ingredients[2] = ingredient3;
-        this.ingredients[3] = ingredient4;
+    public Gericht()	{
+
     }
 
     public void newIngredient(String ingredient)	{
@@ -28,6 +24,10 @@ class Gericht  implements Serializable{
 
     public String getDescription()	{
         return description;
+    }
+
+    public void setName(String name)    {
+        this.Gericht = name;
     }
 
     public String getGericht()	{
