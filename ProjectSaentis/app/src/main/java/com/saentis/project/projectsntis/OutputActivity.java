b6dd9b26eefd571e2d4c6a01599dd289a502e9a0
Max Appel <scheresteinpapier2.0@gmail.com>
2016-11-03 11:56:45 +0100

    ingredients get checked now

--- ProjectSaentis/app/src/main/java/com/saentis/project/projectsntis/OutputActivity.java
+++ ProjectSaentis/app/src/main/java/com/saentis/project/projectsntis/OutputActivity.java
@@ -10,4 +10,16 @@ public class OutputActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_output);
     }
+
+    Init rpdb = new Init(); //recipedatabse
+    String cingr[];
+
+    public boolean testrecipe(int id)   {
+        if(((rpdb.gericht.get(id).getIngredient(0) == cingr[0] || rpdb.gericht.get(id).getIngredient(0) == cingr[1] || rpdb.gericht.get(id).getIngredient(0) == cingr[2] || rpdb.gericht.get(id).getIngredient(0) == cingr[3] ) && (rpdb.gericht.get(id).getIngredient(1) == cingr[0] || rpdb.gericht.get(id).getIngredient(1) == cingr[1] || rpdb.gericht.get(id).getIngredient(2) == cingr[2] || rpdb.gericht.get(id).getIngredient(1) == cingr[3]) && (rpdb.gericht.get(id).getIngredient(2) == cingr[0] || rpdb.gericht.get(id).getIngredient(2) ==cingr[1] || rpdb.gericht.get(id).getIngredient(2) == cingr[3]) && (rpdb.gericht.get(id).getIngredient(3) == cingr[0] || rpdb.gericht.get(id).getIngredient(3) == cingr[1] || rpdb.gericht.get(id).getIngredient(3) == cingr[2] || rpdb.gericht.get(id).getIngredient(3) == cingr[3])))   {
+            return true;
+        }
+        else    {
+            return false;
+        }
+    }
 }
