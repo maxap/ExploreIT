6598bf023b03e981d4a97c302ebf9c4144af3265
Max Appel <maxap@users.noreply.github.com>
2016-11-03 10:56:19 +0100

    Update SelectionActivity.java

--- ProjectSaentis/app/src/main/java/com/saentis/project/projectsntis/SelectionActivity.java
+++ ProjectSaentis/app/src/main/java/com/saentis/project/projectsntis/SelectionActivity.java
@@ -6,10 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class SelectionActivity extends AppCompatActivity {
-
+    EditText ioOne, ioTwo, ioThree, ioFour;
     AutoCompleteTextView autoCompleteTextView1, autoCompleteTextView2, autoCompleteTextView3, autoCompleteTextView4;
     String [] Ingredients;
 
@@ -19,8 +19,7 @@ public class SelectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
 
-        Intent intent;
-        intent = new Intent(this, OutputActivity.class);
+
 
         autoCompleteTextView1 = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
         autoCompleteTextView2 = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView2);
@@ -32,10 +31,29 @@ public class SelectionActivity extends AppCompatActivity {
         autoCompleteTextView2.setAdapter(adapter);
         autoCompleteTextView3.setAdapter(adapter);
         autoCompleteTextView4.setAdapter(adapter);
+        ioOne = (EditText) findViewById(R.id.autoCompleteTextView1);
+        ioTwo = (EditText) findViewById(R.id.autoCompleteTextView2);
+        ioThree = (EditText) findViewById(R.id.autoCompleteTextView3);
+        ioFour = (EditText) findViewById(R.id.autoCompleteTextView4);
+
     }
 
-    public void test(View view)
-    {
-        Toast.makeText(this, autoCompleteTextView1.getText(), Toast.LENGTH_SHORT).show();
+    public static final String INPUT_INGREDIENT1 = "ProjectSaentis.INPUT_INGREDIENT1";
+    public static final String INPUT_INGREDIENT2 = "ProjectSaentis.INPUT_INGREDIENT2";
+    public static final String INPUT_INGREDIENT3 = "ProjectSaentis.INPUT_INGREDIENT3";
+    public static final String INPUT_INGREDIENT4 = "ProjectSaentis.INPUT_INGREDIENT4";
+
+    public void goToOutput(View view)  {
+        Intent intent = new Intent(this, OutputActivity.class);
+        String inputOne = ioOne.getText().toString();
+        String inputTwo = ioTwo.getText().toString();
+        String inputThree = ioThree.getText().toString();
+        String inputFour = ioFour.getText().toString();
+        intent.putExtra(INPUT_INGREDIENT1, inputOne);
+        intent.putExtra(INPUT_INGREDIENT2, inputTwo);
+        intent.putExtra(INPUT_INGREDIENT3, inputThree);
+        intent.putExtra(INPUT_INGREDIENT4, inputFour);
+        startActivity(intent);
+
     }
 }
